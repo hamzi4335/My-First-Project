4250e5be01da606a098ff416f48cca5a92460705
hamzi4335 <hamzafahim2@gmail.com>
2025-08-02 21:53:24 +0500

    final update

--- Rock_papar_Scissor.py
+++ Rock_papar_Scissor.py
@@ -7,21 +7,19 @@ comp_choice = random.choice(item_list)
 print(f"User_Choice = {user_choice}  Computer_choice = {comp_choice}")
 
 if user_choice == comp_choice:
-    print("Both chooses same : Match Tie")
+        print("Both chose the same: Match Tie!")
 elif user_choice == "Rock":
-    if comp_choice == "Papar":
-        print("Papar covers Rock = Computer Win")
-    else:
-        print("Rock smashes Scissor = you win")
-
-elif user_choice == "Papar":
-    if comp_choice == "Scissor":
-        print("Scissor cuts papar = Computer Win")
-    else:
-        print("Paper covers rock = You win")
-
+        if comp_choice == "Paper":
+            print("Paper covers Rock: Computer Wins!")
+        else:
+            print("Rock smashes Scissor: You Win!")
+elif user_choice == "Paper":
+        if comp_choice == "Scissor":
+            print("Scissor cuts Paper: Computer Wins!")
+        else:
+            print("Paper covers Rock: You Win!")
 elif user_choice == "Scissor":
-    if comp_choice == "Papar":
-        print("Scissor cuts papar : You win")
-    else:
-        print("Rock smashes Scissor : Computer WIN")
+        if comp_choice == "Paper":
+            print("Scissor cuts Paper: You Win!")
+        else:
+            print("Rock smashes Scissor: Computer Wins!")
